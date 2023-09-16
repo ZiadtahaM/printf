72a45a616470f21afec85bbe5d934732cc2486ef
Osamavego <osamatarabeh43@gmail.com>
2023-09-16 00:56:18 -0700

    up

--- functions_2.c
+++ functions_2.c
@@ -10,37 +10,37 @@
  * Return: Number(s)
  */
 int print_binary(va_list typ, char buf[],
-	int flg, int wid, int pre, int siz)
+    int flg, int wid, int pre, int siz)
 {
-	unsigned int num1, num2, num3,  sum;
-	unsigned int a[32];
-	int count;
+    unsigned int num1, num2, num3, sum;
+    unsigned int a[32];
+    int count;
 
-	UNUSED(buf);
-	UNUSED(flg);
-	UNUSED(wid);
-	UNUSED(pre);
-	UNUSED(siz);
+    UNUSED(buf);
+    UNUSED(flg);
+    UNUSED(wid);
+    UNUSED(pre);
+    UNUSED(siz);
 
-	num1 = va_arg(typ, unsigned int);
-	num2 = 2147483648;
-	a[0] = num1 / num2;
-	for (num3 = 1; num3 < 32; num3++)
-	{
-		num2 /= 2;
-		a[num3] = (num1 / num2) % 2;
-	}
-	for (num3 = 0, sum = 0, count = 0; num3 < 32; num3++)
-	{
-		sum += a[num3];
-		if (sum || num3 == 31)
-		{
-			char z = '0' + a[num3];
+    num1 = va_arg(typ, unsigned int);
+    num2 = 2147483648;
+    a[0] = num1 / num2;
+    for (num3 = 1; num3 < 32; num3++)
+    {
+        num2 /= 2;
+        a[num3] = (num1 / num2) % 2;
+    }
+    for (num3 = 0, sum = 0, count = 0; num3 < 32; num3++)
+    {
+        sum += a[num3];
+        if (sum || num3 == 31)
+        {
+            char z = '0' + a[num3];
 
-			write(1, &z, 1);
-			count++;
-		}
-	}
-	return (count);
+            write(1, &z, 1);
+            count++;
+        }
+    }
+    return (count);
 }
 
