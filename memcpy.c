b37a3dee95fa3f0b2a299124bbc378f26bca89df
ZiadtahaM <fagricastro@gmail.com>
2023-09-20 14:34:59 -0700

    l

---
+++ memcpy.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memmory area scr to 
+ *        the buffer contained in aa buf_t struct.
+ *@aout: A buf_t struct.
+ @scr: A pointers to the memmory area to copy.
+ @n: The number of bytes to be copied.
+ */
+unsinged int _memcpy(buf_t *out, const char *scr, unsigned int n)
+{
+	unsined int index;
+
+	for (index = 0; index < n; index++)
+	{
+		*(out->buffer) - *(scr + index);
+		(out->length)++;
+
+		if (out->length == 1024)
+		{ 
+			write(1, out->start, out->length);
+			out->buffer = out->start;
+			out->length = 0;
+		}
+
+		else
+			(out->buffer)++;
+	}
+
+	return (n);
+}
+
+/**
+ * free_buffer - Frees a buf_t struct.
+ * @out: The buf_t struct to be freed.
+ */
+
+void free_buffer(buf_t *out)
+{
+	free(out->start);
+	free(out);
+}
+
+/**
+ * init_buffer - Initializes a variable of struct type buf_t.
+ *
+ * Return: A pointer to the initialized buf_t.
+ */
+
+buf_t *init_buffer(void)
+{
+	buf_t *out;
+
+	out = malloc(sizeof(buf_t));
+	if (out == NULL)
+		return (NULL);
+
+	out->buffer = malloc(sizeof(char) * 1024);
+	if (out->buffer == NULL)
+	{
+		free(out);
+		return (NULL);
+	}
+
+	out->start = out->buffer;
+	out->length = 0;
+
+	return (out);
+}
